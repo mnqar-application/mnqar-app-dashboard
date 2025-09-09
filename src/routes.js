67605e6c3fc8de18a586e3382e40a228bf1a6416
Waad-Raggam <waadsraggam@gmail.com>
2025-09-09 20:18:05 +0300

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import OffersTable from './layout/OffersTable'
 import ComplaintsTable from './layout/ComplaintsTable'
 import TransfersTable from './layout/TransfersTable'
 
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
+// const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Colors = React.lazy(() => import('./views/theme/colors/Colors'))
 const Typography = React.lazy(() => import('./views/theme/typography/Typography'))
 const ResetPassword = React.lazy(() => import('./views/pages/reset-password/ResetPassword'))
@@ -58,7 +58,7 @@ const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+  // { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', element: Colors },
   { path: '/theme/typography', name: 'Typography', element: Typography },
